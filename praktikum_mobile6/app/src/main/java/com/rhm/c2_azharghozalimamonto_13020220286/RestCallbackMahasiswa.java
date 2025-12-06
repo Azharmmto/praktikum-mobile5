62bd441f15f588194f34924d98ed13abffe94ebe
Azhar Mamonto <81552113+Azharmmto@users.noreply.github.com>
2025-12-06 08:29:25 +0700

    Add files via upload
    Menambahkan file praktikum 6

---
+++ praktikum_mobile6/app/src/main/java/com/rhm/c2_azharghozalimamonto_13020220286/RestCallbackMahasiswa.java
@@ -0,0 +1,10 @@
+package com.rhm.c2_azharghozalimamonto_13020220286;
+
+import java.util.ArrayList;
+
+public interface RestCallbackMahasiswa {
+    void requestDataMhsSuccess(ArrayList<Mahasiswa> arrayList);
+
+}
+
+
